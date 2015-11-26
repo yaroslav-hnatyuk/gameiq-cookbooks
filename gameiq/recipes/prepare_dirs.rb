2132a26e0357d30621390fde876f3e0ae8b8dc55
yhnatyuk <yaroslav.hnatyuk@gmail.com>
2015-11-27 00:07:26 +0200

    Updated prepare_dirs recipe

--- gameiq/recipes/prepare_dirs.rb
+++ gameiq/recipes/prepare_dirs.rb
@@ -1,23 +1,27 @@
 directory '/var/www/cgi-bin' do
   owner 'ec2-user'
+  group 'ec2-user'
   mode '0755'
   action :create
 end 
 
 directory '/var/www/error' do
   owner 'ec2-user'
+  group 'ec2-user'
   mode '0755'
   action :create
 end
 
 directory '/var/www/html' do
   owner 'ec2-user'
+  group 'ec2-user'
   mode '0777'
   action :create
 end
 
 directory '/var/www/noindex' do
   owner 'ec2-user'
+  group 'ec2-user'
   mode '0755'
   action :create
 end
