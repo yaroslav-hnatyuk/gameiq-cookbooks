aea2a06df5a019a8703162911e2baff45ebcdd4b
yhnatyuk <yaroslav.hnatyuk@gmail.com>
2015-11-27 01:02:07 +0200

    updated composer recipe

--- gameiq/recipes/composer.rb
+++ gameiq/recipes/composer.rb
@@ -1,14 +1,4 @@
-execute "move_into_project_dir" do
-    command "cd /var/www/html/public"
+execute "run_composer" do
+    command "cd /var/www/html/public && /usr/local/bin/composer require friendsofcake/bootstrap-ui:~0.3 && /usr/local/bin/composer install"
     action :run
-end 
-
-execute "composer_require_bootstrap" do
-    command "/usr/local/bin/composer require friendsofcake/bootstrap-ui:~0.3"
-    action :run
-end 
-
-execute "composer_install" do
-    command "/usr/local/bin/composer install"
-    action :run
-end 
+end
