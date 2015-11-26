a9f884bae8277331b163bf0a5ee678b664ec7185
yhnatyuk <yaroslav.hnatyuk@gmail.com>
2015-11-26 15:59:31 +0200

    Updated checkout recipe

--- gameiq/recipes/checkout.rb
+++ gameiq/recipes/checkout.rb
@@ -1,4 +1,4 @@
 execute "checkout_gameiq_app" do
-    command "cd /var/www/html && git clone git@github.com:gameiqinc/public.git && cd /var/www/html/public && git checkout v1.0"
+    command "cd /var/www/html && sudo -u ec2-user git clone git@github.com:gameiqinc/public.git && cd /var/www/html/public && git checkout v1.0"
     action :run
 end
