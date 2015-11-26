9714c849126c1353e682585d95d5a688cdb9792d
yhnatyuk <yaroslav.hnatyuk@gmail.com>
2015-11-27 00:43:57 +0200

    Updated checkout recipe

--- gameiq/recipes/checkout.rb
+++ gameiq/recipes/checkout.rb
@@ -1,4 +1,11 @@
-git '/var/www/html' do
+directory '/var/www/html/public' do
+  owner 'ec2-user'
+  group 'ec2-user'
+  mode '0755'
+  action :create
+end 
+
+git '/var/www/html/public' do
    repository 'git@github.com:gameiqinc/public.git'
    revision 'v1.0'
    action :sync
